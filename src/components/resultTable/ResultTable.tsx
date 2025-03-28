c9e253def6f90e25a7d387e3df26cd978bd52676
ChuChucky <slardakoo@gmail.com>
2025-03-28 15:22:45 +0300

    update

--- src/components/resultTable/ResultTable.tsx
+++ src/components/resultTable/ResultTable.tsx
@@ -7,7 +7,7 @@ import styles from "./style.module.scss";
 import SearchInput from "./searchInput/SearchInput";
 import { store } from "../../store/store"; // Импорт MobX-стора
 import { useNavigate } from "react-router";
-import DropDownDowload from "./dropDownDowload/DropDownDowload";
+import DropDownDowload from "./dropDownDowload/dropDownDowload";
 
 
 type TableRowSelection<T extends object = object> = TableProps<T>["rowSelection"];
