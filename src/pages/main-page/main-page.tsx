3b5e64a2223646d9ddbb44caece06b6da126c228
Silentium237 <vel7527727@gmail.com>
2025-04-02 12:43:50 +0300

    logoм3

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -62,7 +62,7 @@ const MainPage = observer(() => {
       status: 'завершен',
       statusColor: 'warning',
       createDate: '2024.11.07',
-      testName: 'Тест на Алёшу',
+      testName: 'Тест на Валеру',
       testDescription: 'нет описания',
       procent: '21.1',
       completed: '31',
