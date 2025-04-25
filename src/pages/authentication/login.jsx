5c775731c1def132b6420ce0c2d9ac668e524fd9
Silentium237 <vel7527727@gmail.com>
2025-04-25 09:46:51 +0300

    added login

---
+++ src/pages/authentication/login.jsx
@@ -0,0 +1,30 @@
+// material-ui
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+// project import
+import AuthWrapper from './AuthWrapper';
+import AuthLogin from './auth-forms/AuthLogin';
+
+// ================================|| LOGIN ||================================ //
+
+export default function Login() {
+  return (
+    <AuthWrapper>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+            <Typography variant="h3">Авторизация</Typography>
+            <Typography onClick={() => alert('Аккаунт как в Битрикс')} variant="body1" sx={{ textDecoration: 'none' }} color="primary">
+              У Вас отсутствует аккаунт?
+            </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <AuthLogin />
+        </Grid>
+      </Grid>
+    </AuthWrapper>
+  );
+}
